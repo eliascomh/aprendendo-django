96e6ea21852803601676b0baa73449a1d9d0dd84
eliascomh <grilocamusiano@gmail.com>
2022-08-31 17:59:19 -0300

    atualização, erro ao criar migration

--- aplicacao/receitas/models.py
+++ aplicacao/receitas/models.py
@@ -1,3 +1,16 @@
+from codecs import backslashreplace_errors
+from datetime import datetime
+from pyexpat import model
+from tkinter.tix import Tree
+from xml.sax import default_parser_list
 from django.db import models
 
-# Create your models here.
+
+class Receita(models.Model):
+    nome_receita = models.CharField(max_length=200)
+    ingredientes = models.TextChoices()
+    modo_preparo = models.TextChoices()
+    tempo_preparo = models.IntegerField()
+    rendimento = models.TextField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    date_receita = models.DateField(default=datetime.now, blank=True)
