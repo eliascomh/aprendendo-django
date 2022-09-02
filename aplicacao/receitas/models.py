f020a4bde05e71979b4a36e9ceeea828200696f1
eliascomh <grilocamusiano@gmail.com>
2022-09-02 15:34:58 -0300

    atualização

--- aplicacao/receitas/models.py
+++ aplicacao/receitas/models.py
@@ -1,16 +1,12 @@
-from codecs import backslashreplace_errors
-from datetime import datetime
-from pyexpat import model
-from tkinter.tix import Tree
-from xml.sax import default_parser_list
 from django.db import models
+from datetime import datetime
 
 
 class Receita(models.Model):
     nome_receita = models.CharField(max_length=200)
-    ingredientes = models.TextChoices()
-    modo_preparo = models.TextChoices()
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField()
     tempo_preparo = models.IntegerField()
-    rendimento = models.TextField(max_length=100)
+    rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
-    date_receita = models.DateField(default=datetime.now, blank=True)
+    data_receita = models.DateTimeField(default=datetime.now, blank=True)
