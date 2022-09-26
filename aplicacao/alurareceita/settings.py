18db6c9c2a4f6b0955beba1b385cb39db5bf0705
eliascomh <grilocamusiano@gmail.com>
2022-09-26 15:48:40 -0300

    Refatoração de código: apps

--- aplicacao/alurareceita/settings.py
+++ aplicacao/alurareceita/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +143,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     messages.SUCCESS: 'success',
 }
+
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, '../apps'))
